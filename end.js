02624bc1fde51646173a4acb6d802fa18171bbe7
Ahmed Mohamed <143887762+AlbashengineerAhmed@users.noreply.github.com>
2023-09-22 14:51:16 +0300

    Update end.js
    update route

--- end.js
+++ end.js
@@ -25,5 +25,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('https://albashengineerahmed.github.io/Quiz-App/');
+    window.location.assign('/Quiz-App');
 };
